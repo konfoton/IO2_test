cb44a18c8f8fa1444d50af89e015444a6e7414e5
konfoton <konrad.burdach@gmail.com>
2026-03-09 09:16:28 +0100

    more appropriate test

--- test/example_test.cpp
+++ test/example_test.cpp
@@ -5,19 +5,19 @@
 #include <tuple>
 #include "gpu.hpp"
 
-class GPUTest : public ::testing::TestWithParam<std::tuple<double>> {
+class GPUTest : public ::testing::TestWithParam<std::tuple<int>> {
    protected:
   GPU gpu;
 };
 
 
 TEST_P(GPUTest, Working) {
-  float hourlyRate = std::get<0>(GetParam());
+  int mliseconds = std::get<0>(GetParam());
   gpu.Start();
-  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(mliseconds));
   gpu.Stop();
-  EXPECT_GE(gpu.GetTotalRunTimeMs(), 2000.0);
-  EXPECT_LE(gpu.GetTotalRunTimeMs(), 2100.0);
+  EXPECT_GE(gpu.GetTotalRunTimeMs(), static_cast<double>(mliseconds));
+  EXPECT_LE(gpu.GetTotalRunTimeMs(), static_cast<double>(mliseconds) + 100.0);
 }
 
   
@@ -25,12 +25,14 @@ INSTANTIATE_TEST_SUITE_P(
     PowerTests,
     GPUTest,
     ::testing::Values(
-        std::make_tuple(2.0),
-        std::make_tuple(5.0),
-        std::make_tuple(3.0),
-        std::make_tuple(2.0)
+        std::make_tuple(1000),
+        std::make_tuple(1500),
+        std::make_tuple(3000),
+        std::make_tuple(2000)
     )
 );
+
+
 
 int main() {
   ::testing::InitGoogleTest();
